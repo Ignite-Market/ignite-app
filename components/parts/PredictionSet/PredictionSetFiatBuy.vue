df4c86f4f97ca2ad50096fbe6f194f9459d2623b
Simon Šenk <simon.senk@kalmia.si>
2025-06-13 15:22:46 +0200

    basic quote

---
+++ components/parts/PredictionSet/PredictionSetFiatBuy.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useAccount } from '@wagmi/vue';
+
+defineProps<{
+  loading: boolean;
+}>();
+
+const { isConnected } = useAccount();
+const { getQuote } = useThirdwebPay();
+</script>
+
+<template>
+  <div>
+    <BasicButton
+      class="w-full"
+      :btn-class="['!font-bold']"
+      :size="'large'"
+      :disabled="!isConnected"
+      type="secondary"
+      :loading="loading"
+      @click="getQuote('0.01')"
+    >
+      Thirdweb Pay
+    </BasicButton>
+  </div>
+</template>
